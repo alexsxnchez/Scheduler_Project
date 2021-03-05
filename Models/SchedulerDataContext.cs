b12ac1558ab0520c44e1c67f0e3c4c808bc799be
Alexis Arevalo <alexgian1997@gmail.com>
2021-03-05 00:40:15 -0500

    Added Account Authentication

--- Models/SchedulerDataContext.cs
+++ Models/SchedulerDataContext.cs
@@ -4,14 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 using System.Web.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Scheduler_Project.Models
 {
-    public class SchedulerDataContext : DbContext
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
     {
-        public SchedulerDataContext() : base("name=SchedulerDataContext")
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+    public class SchedulerDataContext : IdentityDbContext<ApplicationUser>
+    {
+        public SchedulerDataContext() : base("name=SchedulerDataContext", throwIfV1Schema: false)
         {
 
+        }
+        public static SchedulerDataContext Create()
+        {
+            return new SchedulerDataContext();
         }
         public DbSet<Project> Projects { get; set; }
         public DbSet<Category> Categories { get; set; }
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Scheduler_Project.Startup))]
+namespace Scheduler_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
